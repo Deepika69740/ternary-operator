8277258d7400ff363c8a864d9ba476bdbafa42cb
Deepika69740 <deepikareddyyannam6@gmail.com>
2024-12-01 14:57:46 +0530

    by using ternary oerator

---
+++ index.js
@@ -0,0 +1,26 @@
+let a=prompt("enter your agre here: ");
+if(a>=18){
+    console.log("Your elegible to vote");//if you enter more than are equal to 18 then your elegible for vote
+}else{
+    console.log("your not elegible to vote")//if you enter less than 18 then u are not elegible for vote
+}
+let person=prompt("enter person age here: ")
+if(person>=18){
+    console.log("your eligible to apply for driving license")//if you enter more than are equal to 18 then your elegible for driving license
+}else{
+    console.log("your not elegible to apply for driving license")//if enter yuor age less than 18 then your not elegible for driving license
+}
+let m=prompt("enter males age: ")
+let male=m>=22 ? console.log("eligible"):console.log("not eligible")//if male greter then are equal to 22 then elegible for marragie
+let f=prompt("enter females age: ")
+let female=f>=18 ? console.log("eligible"):console.log("not eligible")//if female greter then are equal to 22 then elegible for marragie
+
+let F=prompt("Enter how number of days you attend: ");
+var G=a/300;
+if (G*100>=75){
+    console.log("eleg")
+}else{
+    console.log("not")
+}
+let r=prompt("enter your age: ")
+let z=r>=60?console.log("qualifies for a senior citizen discount"):console.log("Not qualifies for a senior citizen discount")
